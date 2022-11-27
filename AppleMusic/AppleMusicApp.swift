3da9ccc53ea3429d174cacfadd7e6e59a15b5e34
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-11-27 20:29:53 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Радик Ахметзянов on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
