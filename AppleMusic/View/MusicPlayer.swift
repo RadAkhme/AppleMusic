a64f9ecdb273c41bac64b0b9d48df585eb382423
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-12-02 22:44:30 +0300

    Add MusicPlayer

--- AppleMusic/View/MusicPlayer.swift
+++ AppleMusic/View/MusicPlayer.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct MusicPlayer: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Rectangle()
+                    .frame(height: 65)
+                    .foregroundColor(.primary)
+                    .opacity(0.04)
+                HStack {
+                    Image("album")
+                        .resizable()
+                        .frame(width: 55, height: 55)
+                        .cornerRadius(10)
+                        .padding(.leading)
+                    Text("Flume - Ecdysis")
+                        .font(.system(size: 20))
+                        .padding(.leading, 5)
+                    Spacer()
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                    Image(systemName: "forward.fill")
+                        .resizable()
+                        .frame(width: 35, height: 18)
+                        .padding()
+                }
+            }
+        }
     }
 }
 
