867f3c99815e73e2b51e20f3433297c7e700d029
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-12-02 22:43:25 +0300

    Add LibraryView

---
+++ AppleMusic/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  AppleMusic
+//
+//  Created by Радик Ахметзянов on 01.12.2022.
+//
+
+import Foundation
---
+++ AppleMusic/View/MusicPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  MusicPlayer.swift
+//  AppleMusic
+//
+//  Created by Радик Ахметзянов on 02.12.2022.
+//
+
+import SwiftUI
+
+struct MusicPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MusicPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        MusicPlayer()
+    }
+}
