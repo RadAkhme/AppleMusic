1f7f5eda5d784d204101182f93df84f589e98a37
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-12-04 22:10:28 +0300

    Add RadioView

---
+++ AppleMusic/View/RadioView/BottomRadioView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomRadioView.swift
+//  AppleMusic
+//
+//  Created by Радик Ахметзянов on 04.12.2022.
+//
+
+import SwiftUI
+
+struct BottomRadioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomRadioView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomRadioView()
+    }
+}
