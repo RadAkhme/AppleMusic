b242df3f43a6a8c6717404c23c9260329f89ce7e
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-12-22 22:24:35 +0300

    Add SearchModel and refactory other model

--- AppleMusic/Model/RadioModel.swift
+++ AppleMusic/Model/RadioModel.swift
@@ -1,27 +1,21 @@
 //
-//  Model.swift
+//  RadioModel.swift
 //  AppleMusic
 //
-//  Created by Радик Ахметзянов on 01.12.2022.
+//  Created by Радик Ахметзянов on 22.12.2022.
 //
 
 import Foundation
-import SwiftUI
-
-struct ListItemModel: Identifiable {
-    let id: String = UUID().uuidString
-    let text: String
-    let icon: String
-    var isSelected: Bool
-}
 
 struct RadioModel: Identifiable {
     let id: String = UUID().uuidString
-    let title: String
-    let description: String
-    let image: String
-    
-    static var radioItems = [
+    var title: String
+    var description: String
+    var image: String
+}
+
+class RadioItemsModel: ObservableObject {
+    @Published var radioItems = [
         RadioModel(title: "Полное погружение", description: "Dissolve by Absofacto - Ukulele chords songs", image: "radio4"),
         RadioModel(title: "Домашняя волна", description: "Frank Ocean Announces Homer Radio", image: "radio5"),
         RadioModel(title: "Избранное кантри", description: "Apple Music Country showcases", image: "radio1"),
